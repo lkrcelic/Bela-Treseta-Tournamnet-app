c1473f3d476e3f2d886433517ebe6363d7442763
lkrcelic <lkrcelic@croz.net>
2024-10-01 17:58:41 +0200

    match design take 2

--- src/app/match/ui/ResultsDisplay.tsx
+++ src/app/match/ui/ResultsDisplay.tsx
@@ -7,12 +7,12 @@ export default function ResultsDisplay() {
     const {results} = useMatchStore();
 
     return (
-            <Grid container spacing={2} justifyContent="center">
+            <Grid container spacing={1.5} justifyContent="center">
                 {results.map((result, index) => (
                     <Grid key={index} container item size={{ xs: 10 }} justifyContent="space-evenly" alignItems="center"
-                          sx={{ backgroundColor: "secondary.main", borderRadius: "20px" }}>
+                          sx={{ backgroundColor: "secondary.main", borderRadius: "20px" , paddingY: 0.5 }}>
                         <Grid item size={{ xs: 4 }}>
-                            <Typography variant="h4" textAlign="center">
+                            <Typography variant="h4" textAlign="center" color={result.team1Points > result.team2Points ? "team1.main" :"default"}>
                                 {result.team1Points}
                             </Typography>
                         </Grid>
@@ -24,7 +24,7 @@ export default function ResultsDisplay() {
                         </Grid>
 
                         <Grid item size={{ xs: 4 }}>
-                            <Typography variant="h4" textAlign="center">
+                            <Typography variant="h4" textAlign="center" color={result.team2Points > result.team1Points ? "team2.main" :"default"}>
                                 {result.team2Points}
                             </Typography>
                         </Grid>
