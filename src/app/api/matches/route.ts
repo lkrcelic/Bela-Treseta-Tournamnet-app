c45f47beb69b8bee57660f1e741c70981a378579
Ivan Maretić <I.Mareticcccc@gmail.com>
2024-11-02 20:01:55 +0100

    Update teamscores on match insert

--- src/app/api/matches/route.ts
+++ src/app/api/matches/route.ts
@@ -7,42 +7,43 @@ import {MatchRequestValidation} from "@/app/interfaces/match";
 import {updateRoundWins} from "@/app/lib/service/round/updateWins";
 import {createMatch} from "@/app/lib/service/match/create";
 import {deleteOngoingMatch} from "@/app/lib/service/ongoingMatch/delete";
-import {findOngoingMatchWithResultsAndPlayers} from "@/app/lib/service/ongoingMatch/findOneWithResultsAndPlayers";
-import {
-    findOngoingMatchWithResultsAndAnnouncements
-} from "@/app/lib/service/ongoingMatch/findOneWithResultAndAnnouncements";
+import {findOngoingMatchWithResultsAndAnnouncements} from "@/app/lib/service/ongoingMatch/findOneWithResultAndAnnouncements";
+import {updateScores} from "@/app/lib/updateScores";
 
 export async function GET() {
-    try {
-        // TODO: check user!
-        // TODO?: implement limit/offset
-        const allMatches = await prisma.match.findMany();
+  try {
+    // TODO: check user!
+    // TODO?: implement limit/offset
+    const allMatches = await prisma.match.findMany();
 
-        return NextResponse.json(allMatches, {status: STATUS.OK});
-    } catch (error) {
-        return NextResponse.json({error: "Failed to fetch matches."}, {status: STATUS.ServerError});
-    }
+    return NextResponse.json(allMatches, {status: STATUS.OK});
+  } catch (error) {
+    return NextResponse.json({error: "Failed to fetch matches."}, {status: STATUS.ServerError});
+  }
 }
 
 export async function POST(request: Request) {
-    try {
-        const req_data = await request.json();
-        const ongoingMatchId = MatchRequestValidation.parse(req_data);
+  try {
+    const req_data = await request.json();
+    const ongoingMatchId = MatchRequestValidation.parse(req_data);
 
-        const dbOngoingMatch = await findOngoingMatchWithResultsAndAnnouncements(ongoingMatchId);
+    const dbOngoingMatch = await findOngoingMatchWithResultsAndAnnouncements(ongoingMatchId);
 
-        if (!dbOngoingMatch) {
-            throw new Error("Something went wrong no ongoing match with that id");
-        }
-
-        const match = await createMatch(dbOngoingMatch);
-        await deleteOngoingMatch(ongoingMatchId);
-
-        await updateRoundWins(match.round_id, match.player_pair1_score, match.player_pair2_score)
-
-        return NextResponse.json({message: "Match successfully created"}, {status: STATUS.OK});
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({error: error}, {status: STATUS.BadRequest});
+    if (!dbOngoingMatch) {
+      throw new Error("Something went wrong no ongoing match with that id");
     }
+
+    const match = await createMatch(dbOngoingMatch);
+    await deleteOngoingMatch(ongoingMatchId);
+
+    if (match.round_id) {
+      await updateRoundWins(match.round_id, match.player_pair1_score, match.player_pair2_score);
+      await updateScores(match.round_id);
+    }
+
+    return NextResponse.json({message: "Match successfully created"}, {status: STATUS.OK});
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({error: error}, {status: STATUS.BadRequest});
+  }
 }
